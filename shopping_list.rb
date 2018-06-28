6a7d1aed84d6bceb125e664060e1a2061d1744d0
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-06-27 21:09:53 -0700

    Added print_list() function

--- shopping_list.rb
+++ shopping_list.rb
@@ -17,20 +17,45 @@ end
 
 def add_list_item
     # prompt for item name
-    print "What is the item called?"
+    print "What is the item called? "
 
     # get user input, and return new line
     item_name = gets.chomp
+
+    # ask for quantities
+    puts "How much? "
+
+    # use the to_i method to return user input which is a string to a number
+    quantity = gets.chomp.to_i
     
-    # Create new hash for item
-    hash = { "name" => name}
+    # Create new hash for item_name
+    hash = { "name" => item_name, "quantity" => quantity }
 
     return hash
 
 end
 
+def print_list(list)
+    puts "List: #{list['name']}"
+    puts "----"
+
+    # take each item in this array of list item, and assign it to a variable called "item"
+    list["items"].each do |item|
+        # print out item namd and item quaintity in list item array
+        puts "Item: " + item['name']
+        puts "Quantity: " + item['quantity'].to_s
+        puts "---"
+    end
+end
+
 # call the function
 list = create_list()
-
-# print the hash
 puts list.inspect
+# use push() method to add item into the hash item array
+list['items'].push(add_list_item())
+
+#call the add_list_item, and see it's output
+puts list.inspect
+
+# print out shopping list
+print_list(list)
